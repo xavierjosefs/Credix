244e1f6a99332b3a72ca73523c956e1644f797c8
xavierjosefs <xavierfernandezsantana.com>
2026-05-14 10:22:13 -0400

    prisma pooler fix

--- backend/src/prisma/prisma.ts
+++ backend/src/prisma/prisma.ts
@@ -9,10 +9,18 @@ if (!connectionString) {
   throw new Error('DATABASE_URL is not defined')
 }
 
-const adapter = new PrismaPg({ connectionString })
+const globalForPrisma = globalThis as unknown as {
+  prisma: any
+}
 
-const prisma = new PrismaClient({
-  adapter,
-})
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({ connectionString }),
+  })
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma
+}
 
 export default prisma
